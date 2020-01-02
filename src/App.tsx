af0642f98eba8141a30a980614ec988500048094
Morten Laursen <mortendlaursen@gmail.com>
2020-01-02 16:18:37 +0100

    added description

--- src/App.tsx
+++ src/App.tsx
@@ -30,21 +30,32 @@ class App extends Component<any, AppState> {
             case 0: {
                 return (
                     <div className="App">
-                    <header className="App-header">
-                    Velkommen til Mortens bingo!
-                    </header>
-                    <div className="buttons">
-                    <MyButton handleClick={() => this.setState((prev, props) => {
-                        return {
-                            view: 1
-                        }
-                    })} buttonText="Bingo board"/>
-                    <MyButton handleClick={() => this.setState((prev, props) => {
-                        return {
-                            view: 2
-                        }
-                    })} buttonText="Bingo card"/>
-                    </div>
+                        <header className="App-header">
+                        Velkommen til Mortens bingo!
+                        </header>
+                        <div className="buttons">
+                            <MyButton handleClick={() => this.setState((prev, props) => {
+                                return {
+                                    view: 1
+                                }
+                            })} buttonText="Bingo board"/>
+                            <MyButton handleClick={() => this.setState((prev, props) => {
+                                return {
+                                    view: 2
+                                }
+                            })} buttonText="Bingo card"/>
+                        </div>
+                        <div className="introDiv">
+                            <p>
+                            This is a small project built with react, typescript and bootstrap.
+                            <br/><br/>
+                            You have two choices, bingo board and bingo card.
+                            <br/><br/>
+                            When you view the bingo board, this view is supposed to be put on a large screen for everyone to see. This is the facilitator's (bingo master's) screen. Here the bingo master has the opportunity to draw a random number between 1 and 90, simulating a "normal" bingo game. Furthermore the bingo master's screen has the opportunity to check a bingo plate given a serial number.
+                            <br/><br/>
+                            Everyone besides the bingo master (the players) choose the bingo card option. This generates a random bingo card with 3 rows each containing 5 numbers and a random serial number. This random serial number is used as the seed value to generate the bingo card, and thus when a player yells bingo, they can give the bingo master the serial number for the card, and the card can be checked.
+                            </p>
+                        </div>
                     </div>
                     )
                 }
