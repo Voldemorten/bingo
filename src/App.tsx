5dd8caf2a2e34043db8ad8d4e6373dc7f49826a2
Morten Laursen <mortendlaursen@gmail.com>
2020-01-02 16:28:07 +0100

    renamed react app

--- src/App.tsx
+++ src/App.tsx
@@ -47,13 +47,13 @@ class App extends Component<any, AppState> {
                         </div>
                         <div className="introDiv">
                             <p>
-                            This is a small project built with react, typescript and bootstrap.
+                            This is a small project built with react, typescript and bootstrap. 
                             <br/><br/>
-                            You have two choices, bingo board and bingo card.
+                            When opening the project, you have two choices, bingo board and bingo card.
                             <br/><br/>
-                            When you view the bingo board, this view is supposed to be put on a large screen for everyone to see. This is the facilitator's (bingo master's) screen. Here the bingo master has the opportunity to draw a random number between 1 and 90, simulating a "normal" bingo game. Furthermore the bingo master's screen has the opportunity to check a bingo plate given a serial number.
+                            When you view the bingo board, this view is supposed to be put on a large screen for everyone to see. This is the facilitator's (bingo master's) screen. From here the bingo master is able to draw a random number between 1 and 90, like in a traditional bingo game. Furthermore the bingo master's screen has the opportunity to check a bingo plate given a serial number.
                             <br/><br/>
-                            Everyone besides the bingo master (the players) choose the bingo card option. This generates a random bingo card with 3 rows each containing 5 numbers and a random serial number. This random serial number is used as the seed value to generate the bingo card, and thus when a player yells bingo, they can give the bingo master the serial number for the card, and the card can be checked.
+                            Everyone besides the bingo master (the players) choose the bingo card option. This generates a random bingo card with 3 rows each containing 5 numbers and a random serial number. This random serial number is used as the seed value to generate the bingo card, and thus when a player yells bingo, they can give the bingo master the serial number for the card, and the card can be checked. 
                             </p>
                         </div>
                     </div>
